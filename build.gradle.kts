23eb51674bc479fb98bdce2982d79fade9a56131
Marc Knaup <marc@knaup.io>
2020-08-12 15:19:19 +0100

    🎉 0.9.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 	id("io.fluidsonic.gradle") version "1.1.4"
 }
 
-fluidLibrary(name = "graphql", version = "0.9.3") {
+fluidLibrary(name = "graphql", version = "0.9.4") {
 	allModules {
 		language {
 			withExperimentalApi("io.fluidsonic.graphql.InternalGraphqlApi")
