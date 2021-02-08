96cdfe51668fe3ec94d90af5f94822831a2d4fe4
Marc Knaup <marc@knaup.io>
2021-02-08 02:30:30 +0100

    fluid-gradle 1.1.21

---
+++ sources/Dummy.kt
@@ -0,0 +1,2 @@
+// Publishing breaks if this umbrella project doesn't contain any sources.
+// FileNotFoundException: build/classes/kotlin/iosArm32/main/fluid-graphql.klib (No such file or directory)
