c0ddaf904a3be26c107ef71ab87fd8c897dea798
Marc Knaup <marc@knaup.io>
2021-03-16 18:40:53 +0100

    Updated all dependencies

--- modules/execution/build.gradle.kts
+++ modules/execution/build.gradle.kts
@@ -10,7 +10,7 @@ fluidLibraryModule(description = "FIXME") {
 			}
 
 			testDependencies {
-				implementation(kotlinx("coroutines-core", "1.4.2"))
+				implementation(kotlinx("coroutines-core", "1.4.3"))
 			}
 		}
 
@@ -20,7 +20,7 @@ fluidLibraryModule(description = "FIXME") {
 		js()
 		jvm {
 			testDependencies {
-				implementation(kotlinx("coroutines-test", "1.4.2"))
+				implementation(kotlinx("coroutines-test", "1.4.3"))
 			}
 		}
 	}
