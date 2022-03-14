a020507d970ecf77441da92fd66481ccea35a86e
margino <vadim@widgetlabs.eu>
2022-03-14 21:02:45 +0200

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import io.fluidsonic.gradle.*
 
 plugins {
 	id("io.fluidsonic.gradle") version "1.1.25"
+	id("org.jetbrains.kotlinx.kover") version "0.5.0"
 }
 
 fluidLibrary(name = "graphql", version = "0.11.1") {
@@ -25,4 +26,13 @@ fluidLibraryModule(description = "FIXME") {
 		js()
 		jvm()
 	}
+}
+
+kover {
+	coverageEngine.set(kotlinx.kover.api.CoverageEngine.JACOCO)
+}
+
+tasks.koverMergedXmlReport {
+	xmlReportFile.set(layout.buildDirectory.file("kover/result.xml"))
+	includes = listOf("io.fluidsonic.*")
 }
